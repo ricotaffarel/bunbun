18454b76fe6b3450253f69257f200148e9302007
RICO TAFFAREL ANDI <90165147+ricotaffarel@users.noreply.github.com>
2024-09-11 16:49:39 +0700

    tes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => {
+  let count = 0;
+
+  // Loop 1 juta kali
+  for (let i = 0; i < 10000000; i++) {
+    count++;
+  }
+
+  // Setelah loop selesai, tampilkan pesan
+  return "Hallo Elysia";
+}).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
